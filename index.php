01f01d1372164fda9dbb4fcc2b52ac5a17a44515
Luiz Mendes <developer.luizmendes@gmail.com>
2020-08-24 11:45:08 -0300

    site base

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<?php
+include_once "./base/header.php";
+?>
+
+<!-- Main Header-->
+<header class="main-header style-two">
+
+<?php
+include_once "./base/top.php";
+?>
+<?php
+// include_once "./base/nav.php";
+?>
+       
+</header>
+<!--End Main Header -->
+
+
+
+<?php
+// include_once "./base/bootom.php";
+?>
+<?php
+include_once "./base/footer.php";
+?>
